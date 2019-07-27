851a03390ce9138c6020d9e4c8b0b3674b6b1d19
Jon Ohrt <jonohrt1@gmail.com>
2019-07-26 18:10:35 -0700

    Removed COC

--- .config/coc/extensions/node_modules/coc-json/lib/server/utils/strings.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Determines if haystack ends with needle.
- */
-export declare function endsWith(haystack: string, needle: string): boolean;
-export declare function convertSimple2RegExpPattern(pattern: string): string;
